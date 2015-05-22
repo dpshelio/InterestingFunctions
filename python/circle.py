97456e2ff522fcb94ffdbe79bf8791df3c3d8eaa
Robert Wacholc <robert.wacholc.12@ucl.ac.uk>
2015-05-22 14:54:17 +0100

    Changed to math.pi

--- python/circle.py
+++ python/circle.py
@@ -1,7 +1,7 @@
 ''' Functions from http://math2.org/math/geometry/circles.htm '''
-
+import math
 def circle_area(radius=1):
-    return 3.14*radius**2
+    return math.pi*radius**2
 
 def circle_circunference(radius=1):
     pass
